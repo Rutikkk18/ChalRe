b23e13d09e442fed84581b1a212bf722298933b5
Rutikkk18 <rutikchougale@gmail.com>
2026-01-09 11:24:18 +0530

    cors

--- FRONTEND/chalre/src/api/axios.js
+++ FRONTEND/chalre/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { handleAuthError } from "../utils/errorHandler";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
+  baseURL:"/api"
 });
 
 
