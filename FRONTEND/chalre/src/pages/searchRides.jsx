46ff03078b7518b91c15b97b69ba484d60b7d2a0
Rutikkk18 <rutikchougale@gmail.com>
2026-05-17 13:21:42 +0530

    abc

--- FRONTEND/chalre/src/pages/searchRides.jsx
+++ FRONTEND/chalre/src/pages/searchRides.jsx
@@ -575,7 +575,7 @@ export default function SearchRides() {
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
                 </svg>
-                {isFilterOpen ? t("srCloseFilters") || "Close Filters" : t("srFilters") || "Filters"}
+                {isFilterOpen ? t("Close Filters") || "Close Filters" : t("srFilters") || "Filters"}
               </button>
             </div>
 
